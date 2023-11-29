c7f6e1374ddc27ab602491f27a995e6fc320127f
fawn <fawn@envs.net>
2023-11-29 14:03:00 +0200

    ✨ feature(ping): cuter kaomojis

--- src/commands/misc/ping.rs
+++ src/commands/misc/ping.rs
@@ -4,9 +4,9 @@ use crate::{Context, Error};
 #[poise::command(prefix_command, track_edits, slash_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     let start = std::time::Instant::now();
-    let msg = ctx.send(|r| r.content("( =ω= ) ")).await?;
+    let msg = ctx.send(|r| r.content("(๑˃ᴗ˂)ﻭ")).await?;
     let duration = start.elapsed().as_millis();
-    msg.edit(ctx, |r| r.content(format!("( =ω= ) `{duration}ms`")))
+    msg.edit(ctx, |r| r.content(format!("(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ `{duration}ms`")))
         .await?;
     Ok(())
 }
